67a7185993353502c1b70cf12bf9a1a4ad634c47
x404 <x404@penguin.local>
2026-04-27 11:13:52 -0400

    Remove identity-derived paths from app configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-do
-  local path = vim.env.PATH or ""
-  local wanted = {
-    "/home/x404/.local/bin",
-    "/home/x404/.local/opt/kitty/bin",
-  }
-
-  for i = #wanted, 1, -1 do
-    local dir = wanted[i]
-    if not path:find(dir, 1, true) then
-      path = dir .. ":" .. path
-    end
-  end
-
-  vim.env.PATH = path
-end
-
 require("config.lazy")
